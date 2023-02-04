1784bd6a8497821a24a6d748e965856665d93d52
Elizabeth Pereriadkina <el.pereryadkina@gmail.com>
2023-02-04 23:04:07 +0100

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,35 +1,36 @@
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+const boxes = document.querySelector('#boxes');
+const input = document.querySelector('#controls input');
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+
+createBtn.addEventListener('click', createBoxes);
+destroyBtn.addEventListener('click', destroyBoxes)
 
 
 function createBoxes(amount) {
-  let times = 0;
-  while (times !== amount) {
-    times += 1;
-    let height = 30;
-    let width = 30;
-    const div = document.createElement('div');
-    div.style.backgroundColor = getRandomHexColor();
-    div.style.height = `${height}px`;
-    div.style.width = `${width}px`;
-    height += 10;
-    width += 10;
-
-    console.log(div)
+  amount = Number(input.value);
+  let array = [];
+  function createDiv(didTimes) {
+    if (didTimes !== amount) {
+      const div = document.createElement('div');
+      div.style.backgroundColor = getRandomHexColor();
+      div.style.height = `${30 + didTimes * 10}px`;
+      div.style.width = `${30 + didTimes * 10}px`;
+      array.push(div);
+      didTimes += 1;
+      return createDiv(didTimes);
+    }
   }
-  
+  createDiv(0);
+  boxes.append(...array);
 }
 
-
-// Створи функцію createBoxes(amount), яка приймає один параметр
-//   - число.Функція створює стільки < div >, скільки вказано в 
-//   amount і додає їх у div#boxes.
-
-// Розміри найпершого <div> - 30px на 30px.
-// Кожен елемент після першого повинен бути ширшим і вищим від
-//  попереднього на 10px.
-// Всі елементи повинні мати випадковий колір фону у форматі HEX.
-//  Використовуй готову функцію getRandomHexColor для отримання
-// кольору.
+function destroyBoxes() {
+  boxes.innerHTML = '';
+  input.value = '';
+}

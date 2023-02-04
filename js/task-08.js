92aa418904db4d7cd3de878b5f4a9ba49b169f7f
Elizabeth Pereriadkina <el.pereryadkina@gmail.com>
2023-02-04 20:57:10 +0100

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,3 +1,18 @@
-const submitBtn = document.querySelector('button')
-const inputEmail = document.querySelector()
-console.log(inputEmail)
+const formLink = document.querySelector('.login-form');
+
+formLink.addEventListener('submit', handleSubmit)
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const {
+    elements: { email, password }
+    } = event.currentTarget;
+
+    if (password.value === '' || email.value === '') {
+        return alert('All fields should be filled!');
+    }
+
+    let loginData = { email: email.value, password: password.value };
+    console.log(loginData);
+    event.currentTarget.reset();
+}

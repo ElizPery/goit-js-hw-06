7b0d1caebd02467e0140243f06db370773b042c7
Elizabeth Pereriadkina <el.pereryadkina@gmail.com>
2023-02-04 19:55:26 +0100

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
+
+const changeColorBtn = document.querySelector('.change-color');
+const typeOfColor = document.querySelector('.color');
+
+changeColorBtn.addEventListener('click', changeColor)
+
+function changeColor(event) {
+  document.body.style.backgroundColor = getRandomHexColor();
+  typeOfColor.textContent = document.body.style.backgroundColor;
+} 
